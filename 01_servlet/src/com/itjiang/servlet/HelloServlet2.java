925d83b42c21d72e5915fbe66e0bdfc7a5d4e931
jiangdla <jiangdla@dcits.com>
2021-01-06 14:50:26 +0800

    kk

--- 01_servlet/src/com/itjiang/servlet/HelloServlet2.java
+++ 01_servlet/src/com/itjiang/servlet/HelloServlet2.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 public class HelloServlet2 extends HttpServlet {
     //可自动判定get请求和post请求
     //根据业务需要重写get,post方法
+	//....
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("得到了get请求");
